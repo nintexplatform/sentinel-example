319ae91dd2815e81cb589f0b24ebc37349e98f54
Sai Kambaiyyagari <sai.kambaiyyagari@nintex.com>
2017-09-18 12:16:43 +1000

    feat(env-var): Differentiate the Application under test and SSLyze server url environment variables

--- features/steps/steps.js
+++ features/steps/steps.js
@@ -1,6 +1,8 @@
 module.exports = function () {
+  const autUrl = process.env.AUT_SERVER_URL || '.';
+
   this.Given(/^I have loaded the web application$/, { timeout: 10000 }, async function () {
-    return await this.driver.get('http://nodegoat:4000');
+    return await this.driver.get(`${autUrl}`);
   });
 
   this.Given(/^I have loaded the web application with "([^"]+)"$/, { timeout: 10000 }, async function (url) {
