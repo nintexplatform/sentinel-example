c14d1d1945b2fdbabc4f17cef379dabc313cdaf1
Sai Siddhartha Kambaiyyagari <31752496+sai-kambaiyyagari@users.noreply.github.com>
2017-09-19 13:25:28 +1000

    feat(env-var): Support the sentinel-ast code changes to support  AUT_SERVER_URL and SSLYZE_SERVER_URL env var (#3)
    * feat(env-var): Differentiate the Application under test and SSLyze server url environment variables
    
    * feat(selenium-core): Move the common selenium steps to sentinel-ast repo
    
    * fix(package-ver): Bump up the version of sentinel-ast
    
    * Update steps.js

--- features/steps/steps.js
+++ features/steps/steps.js
@@ -1,9 +1,5 @@
 module.exports = function () {
-  this.Given(/^I have loaded the web application$/, { timeout: 10000 }, async function () {
-    return await this.driver.get('http://nodegoat:4000');
-  });
 
-  this.Given(/^I have loaded the web application with "([^"]+)"$/, { timeout: 10000 }, async function (url) {
-    return await this.driver.get(url);
-  });
+  // Add your Step definitions here
+
 };
